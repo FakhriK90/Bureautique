1c51cabcad6ab57f125e0998ebb5b2eabfa18a14
FakhriK90 <eza3im2009@live.fr>
2023-02-04 11:17:35 +0100

    first commit

--- site-web-bureautique/src/App.js
+++ site-web-bureautique/src/App.js
@@ -2,6 +2,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './components/footer/Footer';
+import HomeJB from './components/homeJB/HomeJB';
 import Navigation from './components/navigation/Navigation';
 import SignIn from './components/signIn/SignIn';
 import SignUp from './components/signUp/SignUp';
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
     <Navigation/>
     <Routes>
+    <Route exact path='/' element={<HomeJB/>} />
     <Route exact path='/signin' element={<SignIn/>}/>
     <Route exact path='/signup' element={<SignUp/>}/>
     </Routes>
